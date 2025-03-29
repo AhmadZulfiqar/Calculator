2375b90c0146a9b555143ce0b69a7ae09306f9b0
AhmadZulfiqar <164607198+AhmadZulfiqar@users.noreply.github.com>
2025-03-29 23:17:50 +0500

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,28 +1,32 @@
-let input=document.querySelector("input");
-let btns=document.querySelectorAll("button");
-let string="";
-let arr=Array.from(btns);
-arr.forEach(btns=>{
-    btns.addEventListener("click",(e)=>{
-        if(e.target.innerHTML =="="){
-            string=eval(string);
-            input.value=string;
+let input = document.querySelector("input");
+let btns = document.querySelectorAll("button");
+let string = "";
+let arr = Array.from(btns);
+
+arr.forEach(btns => {
+    btns.addEventListener("click", (e) => {
+        if (e.target.innerHTML == "=") {
+            try {
+                string = eval(string);
+                input.value = string;
+            } catch {
+                input.value = "Error";
+                string = "";
+            }
+        } else if (e.target.innerHTML == "C") {
+            string = "";
+            input.value = string;
+        } else if (e.target.innerHTML == "DEL") {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        } else if (e.target.innerHTML == "+/-") {
+            if (string) {
+                string = String(-parseFloat(string));
+                input.value = string;
+            }
+        } else {
+            string += e.target.innerHTML;
+            input.value = string;
         }
-        else if(e.target.innerHTML=="C")
-        {
-            string="";
-            input.value=string;
-        }
-        else if(e.target.innerHTML=="DEL")
-        {
-            string=string.substring(0,string.length-1);
-            input.value=string;
-            
-        }
-        else
-        {
-            string+=e.target.innerHTML;
-        input.value=string;
-        }
-    })
-})
+    });
+});
